a6b1a302720ea7fc26210d59ef137a7bd5c83f65
Andys-in <andyjeffersonperez@gmail.com>
2023-04-04 22:56:09 -0400

    some changes

--- back-end-code/index.js
+++ back-end-code/index.js
@@ -1,15 +1,13 @@
 import cors from 'cors'
 import express from 'express'
-// const express = require('express')
-// const cors = require('cors')
 
 const app = express()
 app.use(cors());
 
 
-app.get("/data", (req, res) => {
+app.get("/", (req, res) => {
     res.send('Hello From BacK-End')
 })
 app.listen(5000, ()=> {
-    console.log('Hello from Back-End')
+    console.log('Hey Back-End is running')
 } )
